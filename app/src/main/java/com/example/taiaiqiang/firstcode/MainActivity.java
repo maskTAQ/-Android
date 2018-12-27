27d4ee27c733e0bbc11d57c057808b8397c987c2
邰爱强 <50889574@qq.com>
2018-12-28 00:24:38 +0800

    update

---
+++ app/src/main/java/com/example/taiaiqiang/firstcode/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.taiaiqiang.firstcode;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button sendButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://www.baidu.com"));
+
+        startActivity(intent);
+    }
+}
