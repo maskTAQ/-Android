42d6a35fc39d6145fd06adce2bb504744bb6d577
邰爱强 <50889574@qq.com>
2018-12-28 11:24:07 +0800

    add broadcast

--- app/src/main/java/com/example/taiaiqiang/firstcode/MainActivity.java
+++ app/src/main/java/com/example/taiaiqiang/firstcode/MainActivity.java
@@ -1,30 +1,73 @@
 package com.example.taiaiqiang.firstcode;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button sendButton;
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private IntentFilter intentFilter;
+    private  NetworkChangeReceiver networkChangeReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        networkChangeReceiver =  new NetworkChangeReceiver();
+//        registerReceiver(networkChangeReceiver,intentFilter);
 
+        sendBroad();
+
+
+    }
+    public void sendBroad(){
+        Button button = (Button) findViewById(R.id.send);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"send myBroad",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent("com.example.taiaiqiang.firstcode.myB");
+                intent.setComponent(new ComponentName("com.example.taiaiqiang.firstcode","com.example.taiaiqiang.firstcode.MyBroadcastReceiver"));
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("http://www.baidu.com"));
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
 
-        startActivity(intent);
+    class NetworkChangeReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "onReceive: ");
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+            if(networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context,"network is available",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context,"network is unavailable",Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 }
